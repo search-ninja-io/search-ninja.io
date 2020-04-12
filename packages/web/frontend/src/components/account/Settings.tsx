4ec771b9666cf357b37ebed525d24a1f8be0ab84
André Markwalder <andre.markwalder@gmail.com>
2020-04-12 02:38:03 +0200

    Test with lintd-staged

--- packages/web/frontend/src/components/account/Settings.tsx
+++ packages/web/frontend/src/components/account/Settings.tsx
@@ -1,44 +1,43 @@
-import React, { useState } from "react";
-
-import { ChangePassword } from "./ChangePassword";
-import { Mfa } from "./Mfa";
+import React, { useState } from 'react';
 
 import styled from 'styled-components';
+import { Container, Jumbotron, Row, Tab, Col, Nav } from 'react-bootstrap';
+import { ChangePassword } from './ChangePassword';
+import { Mfa } from './Mfa';
+
 import { MessageBanner, MessageBannerProps } from '../banner/MessageBanner';
-import { Container, Jumbotron, Row, Tab, Col, Nav } from "react-bootstrap";
 
-const Styled = styled.div`
-`;
+const Styled = styled.div``;
 
-export const Settings = () => {
+export const Settings = (): JSX.Element => {
     const [message, setMessage] = useState<MessageBannerProps>({});
 
-    const clearMessage = () => {
+    const clearMessage = (): void => {
         setMessage({});
-    }
+    };
 
     return (
         <Styled>
-
             {message.successes || message.warnings || message.errors ? <MessageBanner {...message} /> : <></>}
 
             <Container className="d-flex mt-5 justify-content-center">
-
                 <Jumbotron className="m-0 p-5 w-100">
-
                     <h1>Settings</h1>
                     <hr />
                     <Container className="m-0 mt-2 p-0">
-
                         <Tab.Container id="settings-nav" defaultActiveKey="change-password">
                             <Row>
                                 <Col sm={3}>
                                     <Nav variant="pills" className="flex-column">
                                         <Nav.Item>
-                                            <Nav.Link onClick={clearMessage} eventKey="change-password">Change Password</Nav.Link>
+                                            <Nav.Link onClick={clearMessage} eventKey="change-password">
+                                                Change Password
+                                            </Nav.Link>
                                         </Nav.Item>
                                         <Nav.Item>
-                                            <Nav.Link onClick={clearMessage} eventKey="mfa">Multi Factor Authentication</Nav.Link>
+                                            <Nav.Link onClick={clearMessage} eventKey="mfa">
+                                                Multi Factor Authentication
+                                            </Nav.Link>
                                         </Nav.Item>
                                     </Nav>
                                 </Col>
@@ -54,13 +53,10 @@ export const Settings = () => {
                                 </Col>
                             </Row>
                         </Tab.Container>
-
                     </Container>
-
                 </Jumbotron>
             </Container>
-
-        </Styled >
+        </Styled>
     );
 };
 
